096fd27f5dda65871b6d0fa30093a53b109a13e1
yyyoonho <parn997@naver.com>
2025-04-05 00:42:34 +0900

    feat: README

--- Utils/MemoryPool/MemoryPool.cpp
+++ Utils/MemoryPool/MemoryPool.cpp
@@ -1,24 +1,37 @@
 ﻿#include <iostream>
 #include <Windows.h>
 
-#include "MemoryPool.h"
-
 using namespace std;
 
+#include "MemoryPool.h"
 
+class CTest
+{
+public:
+    CTest()
+    {
+        cout << "CTEST 생성자 호출" << endl;
+        _x = 0xff;
+    }
+    ~CTest()
+    {
+        cout << "CTEST 소멸자 호출" << endl;
+        _x = 0;
+    }
+
+    int _x;
+    //char _y;
+};
 
 int main()
 {
-    procademy::MemoryPool<int> mp(0);
-    int* a = mp.Alloc();
+    procademy::MemoryPool<CTest> mp(10, true);
 
-    *a = 3;
-    cout << *a;
+    CTest* a = mp.Alloc(); // CTEST 생성자 -> Node 생성자
+    mp.Free(a); // CTest 소멸자
 
-    mp.Free(a);
-
-    int* b = mp.Alloc();
-    cout << *b;
-
-    cout << mp.GetCapacity();
+    CTest* b = mp.Alloc(); // CTEST 생성자
+    //mp.Free(b); // CTest 소멸자
+    
+    int bf2 = 3;
 }
--- Utils/MemoryPool/MemoryPool.h
+++ Utils/MemoryPool/MemoryPool.h
@@ -7,20 +7,19 @@ namespace procademy
 	class MemoryPool
 	{
 	private:
-
-		class Node
+		struct Node
 		{
-		public:
-			Node();
-			~Node();
-
-			DATA* _pData;
-			Node* _next;
+			Node* _underflowGuard = NULL; // 겸 poolId 체크용
+			DATA _data;
+			Node* _pNextNode = NULL; // 겸 _overflowGuard
 		};
 
 	public:
 		MemoryPool(int iBlockNum, bool bPlacement = false);
 		virtual ~MemoryPool();
+
+		/* 테스트 */
+		void Init();
 
 		DATA* Alloc();
 		bool Free(DATA* pData);
@@ -33,18 +32,58 @@ namespace procademy
 		int _useCount = 0;
 
 		Node* _pFreeNode = NULL;
+
+		bool _bPlacement = false;
+
+		__int64 _poolId;
 	};
 	
 
 	template<typename DATA>
 	inline MemoryPool<DATA>::MemoryPool(int iBlockNum, bool bPlacement)
 	{
-		for (int i = 0; i < iBlockNum; i++)
-		{
-			Node* tmpNode = new Node;
+		Init();
 
-			tmpNode->_next = _pFreeNode;
-			_pFreeNode = tmpNode;
+		_bPlacement = bPlacement;
+
+		// 지금 초기 세팅할때 생성자 호출하고, 이후로는 하지 않는다.ex) 직렬화버퍼 풀
+		// 공간 확보 + 생성자 호출
+		if (_bPlacement == false) 
+		{
+			for (int i = 0; i < iBlockNum; i++)
+			{
+				Node* newNode = new Node;
+
+				// nextNode 세팅
+				newNode->_pNextNode = _pFreeNode;
+				_pFreeNode = newNode;
+				
+				// prev 세팅
+				newNode->_underflowGuard = (Node*)_poolId;
+
+			}
+		}
+		// 어차피 Alloc할때마다 생성자 호출할거니, 지금은 생성자 호출 하지않겠다.
+		// 공간만 확보
+		else
+		{
+			for (int i = 0; i < iBlockNum; i++)
+			{
+				// malloc
+				Node* newNode = (Node*)malloc(sizeof(Node));
+				if (newNode == NULL)
+				{
+					cout << "Error malloc" << endl;
+					return;
+				}
+
+				// nextNode 세팅
+				newNode->_pNextNode = _pFreeNode;
+				_pFreeNode = newNode;
+
+				// prev 세팅
+				newNode->_underflowGuard = (Node*)_poolId;
+			}
 		}
 
 		_capacity = iBlockNum;
@@ -53,18 +92,45 @@ namespace procademy
 	template<typename DATA>
 	inline MemoryPool<DATA>::~MemoryPool()
 	{
-		if (_pFreeNode == NULL)
-			return;
-
-		Node* tmpNext;
-
-		while (1)
+		if (_bPlacement == true)
 		{
-			tmpNext = _pFreeNode->_next;
-			delete _pFreeNode;
+			Node* next = NULL;
+			next = _pFreeNode;
+			
+			while (next != NULL)
+			{
+				Node* targetNode = next;
+				next = targetNode->_pNextNode;
 
-			if (tmpNext == NULL)
-				break;
+				free (targetNode);
+			}
+		}
+		else
+		{
+			Node* next = NULL;
+			next = _pFreeNode;
+
+			while (next != NULL)
+			{
+				Node* targetNode = next;
+				next = targetNode->_pNextNode;
+
+				delete targetNode;
+			}
+		}		
+	}
+
+	template<typename DATA>
+	inline void MemoryPool<DATA>::Init()
+	{
+		// TODO: srand는 한번만 해도 되니 수정하자.
+		srand((unsigned int)time(NULL));
+
+		__int64 tmp = (__int64)rand();
+
+		for (int i = 1; i <= 4; i++)
+		{
+			_poolId = _poolId | (tmp << i * 16);
 		}
 	}
 
@@ -73,39 +139,65 @@ namespace procademy
 	{
 		if (_pFreeNode == NULL)
 		{
+			Node* newNode = new Node;
+
+			// overflowGuard 세팅
+			newNode->_pNextNode = (Node*)_poolId;
+
 			_capacity++;
 			_useCount++;
 
-			Node* newNode = new Node;
-
-			Node** tmp = (Node**)((BYTE*)(newNode->_pData) + sizeof(DATA));
-			*tmp = newNode;
-
-			return newNode->_pData;
+			return &(newNode->_data);
 		}
 		else
 		{
+			Node* newNode = _pFreeNode;
+			_pFreeNode = newNode->_pNextNode;
+
+			// overflowGuard 세팅
+			newNode->_pNextNode = (Node*)_poolId;
+
+			if (_bPlacement == true)
+			{
+				new(&(newNode->_data)) DATA();
+			}
+
 			_useCount++;
 
-			Node* nowNode = _pFreeNode;
-			_pFreeNode = nowNode->_next;
-
-			Node** tmp = (Node**)((BYTE*)(nowNode->_pData) + sizeof(DATA));
-			*tmp = nowNode;
-
-			return nowNode->_pData;
+			return &(newNode->_data);
 		}
 	}
 
 	template<typename DATA>
 	inline bool MemoryPool<DATA>::Free(DATA* pData)
 	{
+		Node* tmpNode = (Node*)((BYTE*)pData - sizeof(Node*));
+
+		// 내 풀인지 아닌지 체크 + 언더플로우 체크
+		Node* underflowGuard = tmpNode->_underflowGuard;
+		if (underflowGuard != (Node*)_poolId)
+		{
+			return false;
+		}
+
+		// 오버플로우 체크
+		Node* overflowGuard = tmpNode->_pNextNode;
+		if (overflowGuard != (Node*)_poolId)
+		{
+			return false;
+		}
+
+		tmpNode->_pNextNode = _pFreeNode;
+		_pFreeNode = tmpNode;
+
+		// Alloc할때마다 생성자호출을 해야하기 때문에,
+		// Free로 돌아올 때 소멸자 호출줘야한다.
+		if (_bPlacement == true)
+		{
+			pData->~DATA();
+		}
+
 		_useCount--;
-
-		Node** tmp = (Node**)((BYTE*)pData + sizeof(DATA));
-
-		(*tmp)->_next = _pFreeNode;
-		_pFreeNode = *tmp;
 
 		return true;
 	}
@@ -122,18 +214,6 @@ namespace procademy
 		return _useCount;
 	}
 
-	template<typename DATA>
-	inline MemoryPool<DATA>::Node::Node()
-	{
-		_pData = (DATA*)malloc(sizeof(DATA) + sizeof(Node*));
-		_next = NULL;
-	}
-
-	template<typename DATA>
-	inline MemoryPool<DATA>::Node::~Node()
-	{
-		free(_pData);
-	}
 
 }
 
