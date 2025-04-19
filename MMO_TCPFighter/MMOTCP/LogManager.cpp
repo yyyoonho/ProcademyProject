d617157c70f39fff48aa3db26e8a3d92054904e3
yyyoohno <parn997@naver.com>
2025-04-19 16:34:56 +0900

    feat: LogManger

---
+++ MMO_TCPFighter/MMOTCP/LogManager.cpp
@@ -0,0 +1,24 @@
+#include <Windows.h>
+#include <iostream>
+
+#include "LogManager.h"
+
+using namespace std;
+
+int g_iLogLevel;
+WCHAR g_szLogBuff[1024];
+
+void Log(WCHAR* szString, int iLogLevel)
+{
+	char szFileName[32]; 
+	sprintf_s(szFileName, "%s.txt", __DATE__);
+
+	FILE* fp;
+	fopen_s(&fp, szFileName, "at, ccs=UTF-16LE");
+
+	fwrite(szString, wcslen(szString) * sizeof(WCHAR), 1, fp);
+
+	fclose(fp);
+}
+
+//https://www.kernelpanic.kr/50
--- MMO_TCPFighter/MMOTCP/ServerControl.cpp
+++ MMO_TCPFighter/MMOTCP/ServerControl.cpp
@@ -1,6 +1,60 @@
+#include <Windows.h>
+#include <conio.h>
+
+#include "LogManager.h"
+#include "MMOTCP.h"
 #include "ServerControl.h"
+
+using namespace std;
+
+/*
+	L: 컨트롤 Lock
+	U: 컨트롤 UnLock
+	Q: 서버 종료
+
+	0: 로그레벨 - 디버그
+	1: 로그레벨 - 에러
+	2: 로그레벨 - 시스템
+*/
 
 void ServerControl()
 {
+	static bool bControlMode = false;
+
+	if (_kbhit())
+	{
+		WCHAR ControlKey = _getwch();
+
+		if (L'U' == ControlKey || L'u' == ControlKey)
+		{
+			bControlMode = true;
+		}
+
+		if (L'L' == ControlKey || L'l' == ControlKey)
+		{
+			bControlMode = false;
+		}
+
+		if ((L'Q' == ControlKey || L'q' == ControlKey) && bControlMode)
+		{
+			g_bShutdown = true;
+		}
+
+		if ((L'0' == ControlKey) && bControlMode)
+		{
+			g_iLogLevel = 0;
+
+			_LOG(dfLOG_LEVEL_DEBUG, L"%ls", L"테스트입니다.");
+		}
+		
+		if ((L'1' == ControlKey) && bControlMode)
+		{
+			g_iLogLevel = 1;
+		}
+		if ((L'2' == ControlKey) && bControlMode)
+		{
+			g_iLogLevel = 2;
+		}
+	}
 
 }
