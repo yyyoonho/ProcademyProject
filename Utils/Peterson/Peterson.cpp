979096d3455c1adde0f94da7661958038c75f3eb
yyyoonho <parn997@naver.com>
2025-04-28 01:13:49 +0900

    feat: Peterson 알고리즘 분석

--- Utils/Peterson/Peterson.cpp
+++ Utils/Peterson/Peterson.cpp
@@ -9,6 +9,13 @@ using namespace std;
 LONG g_flag[2];
 LONG g_turn;
 
+LONG flagInterlocked_0 = 4;
+LONG flagInterlocked_1 = 4;
+LONG turnInterlocked_0 = 4;
+LONG turnInterlocked_1 = 4;
+
+bool enterZone[2];
+
 LONG a = 0;
 
 UINT FuncA(LPVOID lpThreadParameter)
@@ -17,24 +24,30 @@ UINT FuncA(LPVOID lpThreadParameter)
     while (count++ < 10000000)
     {
         g_flag[0] = true;
+
         g_turn = 0;
 
         while (1)
         {
-            if (g_flag[1] == false)
+            if ((flagInterlocked_1 = InterlockedCompareExchange(&g_flag[1], 0, 0)) == false)
                 break;
                 
-            if (g_turn != 0)
+            if ((turnInterlocked_0 = InterlockedCompareExchange(&g_turn, 0, 0)) != 0)
                 break;
                 
         }
 
         // 임 계 영 역
-        
+        enterZone[0] = true;
+
+        if (enterZone[1] == true && enterZone[0] == true)
+        {
+            DebugBreak();
+        }
 
         a++;
 
-
+        enterZone[0] = false;
         // 임 계 영 역
 
         g_flag[0] = false;
@@ -49,20 +62,25 @@ UINT FuncB(LPVOID lpThreadParameter)
     while (count++ < 10000000)
     {
         g_flag[1] = true;
+        
         g_turn = 1;
 
         while (1)
         {
             if (g_flag[0] == false)
                 break;
-            if (g_turn != 1)
+            if ((turnInterlocked_1 = InterlockedCompareExchange(&g_turn, 0, 0)) != 1)
                 break;
         }
 
         // 임 계 영 역
+        enterZone[1] = true;
 
         a++;
 
+        Sleep(3);
+
+        enterZone[1] = false;
         // 임 계 영 역
 
         g_flag[1] = false;
@@ -82,4 +100,5 @@ int main()
     WaitForMultipleObjects(2, threadHandles, true, INFINITE);
 
     printf("%d\n", a);
+
 }
