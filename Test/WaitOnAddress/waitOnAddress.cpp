7cbb66bf03b8bd2a7560273665669882b880ae79
yyyoonho <parn997@naver.com>
2025-05-06 17:41:19 +0900

    feat: waitOnAddress

---
+++ Test/WaitOnAddress/waitOnAddress.cpp
@@ -0,0 +1,60 @@
+﻿#pragma comment(lib, "winmm.lib")
+#pragma comment(lib, "CustomProfiler.lib")
+#pragma comment(lib,"ws2_32.lib")
+#pragma comment(lib,"synchronization.lib")
+
+
+#include <iostream>
+#include <winsock2.h>
+#include <WS2tcpip.h>
+#include <windows.h>
+#include <iostream>
+#include <synchapi.h>
+#include <conio.h>
+
+#include <thread>
+#include <process.h>
+
+
+using namespace std;
+
+LONG g_lock = 1; // 0-> 아무도 없음, 1-> 누가 들어가 있음
+LONG g_compare = 1;
+
+CRITICAL_SECTION g_CS;
+
+void Func1()
+{
+	while (1)
+	{
+		cout << "ThreadID: " << GetCurrentThreadId() << endl;
+		//Sleep(100);
+		WaitOnAddress(&g_lock, &g_compare, sizeof(LONG), INFINITE);
+	}
+}
+
+int main()
+{
+	timeBeginPeriod(1);
+
+	HANDLE threadHandles[10];
+	for (int i = 0; i < 10; i++)
+	{
+		threadHandles[i] = (HANDLE)_beginthreadex(NULL, 0, (_beginthreadex_proc_type) & Func1, NULL, NULL, NULL);
+		Sleep(10);
+	}
+
+	cout<<"***********************"<< endl;
+
+	while (1)
+	{
+		if (_getch())
+		{
+			WakeByAddressSingle(&g_lock);
+
+		}
+
+		
+	}
+
+}
---
+++ Test/WaitOnAddress/waitOnAddressTest.cpp
@@ -0,0 +1,74 @@
+﻿#pragma comment(lib, "winmm.lib")
+#pragma comment(lib, "CustomProfiler.lib")
+#pragma comment(lib,"ws2_32.lib")
+#pragma comment(lib,"synchronization.lib")
+
+
+#include <iostream>
+#include <winsock2.h>
+#include <WS2tcpip.h>
+#include <windows.h>
+#include <iostream>
+#include <synchapi.h>
+#include <conio.h>
+
+#include <thread>
+#include <process.h>
+
+
+using namespace std;
+
+LONG g_lock = 0; // 0-> 아무도 없음, 1-> 누가 들어가 있음
+LONG g_compare = 1;
+
+int a = 0;
+
+void Lock()
+{
+	while (1)
+	{
+		int ret = InterlockedExchange(&g_lock, 1);
+		if (ret == 0)
+		{
+			return;
+		}
+		else if (ret == 1)
+		{
+			WaitOnAddress(&g_lock, &g_compare, sizeof(LONG), INFINITE);
+		}
+	}
+}
+
+void UnLock()
+{
+	InterlockedExchange(&g_lock, 0);
+
+	WakeByAddressSingle(&g_lock);
+}
+
+void Func1()
+{
+	for (int i = 0; i < 1000000; i++)
+	{
+		Lock();
+
+		a++;
+
+		UnLock();
+	}
+}
+
+int main()
+{
+	timeBeginPeriod(1);
+
+	HANDLE threadHandles[10];
+	for (int i = 0; i < 10; i++)
+	{
+		threadHandles[i] = (HANDLE)_beginthreadex(NULL, 0, (_beginthreadex_proc_type) & Func1, NULL, NULL, NULL);
+	}
+
+	WaitForMultipleObjects(10, threadHandles, TRUE, INFINITE);
+
+	cout << a;
+}
