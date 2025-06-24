29a14e78e82908930195c31ff9a58ae1eb976909
yyyoohno <parn997@naver.com>
2025-06-24 14:52:54 +0900

    feat: LockFree 스택 초안

---
+++ Utils/LockFreeStack/LockFreeStack.cpp
@@ -0,0 +1,74 @@
+﻿#pragma comment(lib, "winmm.lib")
+
+#include <iostream>
+#include <list>
+#include <Windows.h>
+
+using namespace std;
+
+template <typename T>
+struct Node
+{
+	T data;
+	Node* next;
+};
+
+template <typename T>
+class MyStack
+{
+public:
+	Node<T>* top = NULL;
+
+	void Push(T data);
+	void Pop(T* data);
+};
+
+template<typename T>
+void MyStack<T>::Push(T data)
+{
+	Node<T>* newNode = new Node<T>;
+	newNode->data = data;
+
+	while (1)
+	{
+		Node<T>* oldTop = top;
+		newNode->next = oldTop;
+
+		LONG64 ret = InterlockedCompareExchange64((LONG64*)&top, (LONG64)newNode, (LONG64)oldTop);
+		if ((Node<T>*)ret == oldTop)
+		{
+			break;
+		}
+	}
+}
+
+template<typename T>
+void MyStack<T>::Pop(T* data)
+{
+	while (1)
+	{
+		Node<T>* oldTop = top;
+		Node<T>* newTop = oldTop->next;
+
+		LONG64 ret = InterlockedCompareExchange64((LONG64*)&top, (LONG64)newTop, (LONG64)oldTop);
+		if ((Node<T>*)ret == oldTop)
+		{
+			*data = oldTop->data;
+			delete oldTop;
+			break;
+		}
+	}
+
+	return;
+}
+
+MyStack<int> g_Stack;
+
+int main()
+{
+	timeBeginPeriod(1);
+
+
+
+	return 0;
+}
