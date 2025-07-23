95673d849a65351b098261a20db1e1e747ce4f94
yyyoonho <parn997@naver.com>
2025-07-24 03:01:12 +0900

    feat: 쓰레드 동기화 연습

---
+++ 3Cource/ThreadPractice/ThreadTest1/ThreadTest1.cpp
@@ -0,0 +1,98 @@
+﻿#pragma comment(lib, "winmm.lib")
+
+#include <iostream>
+#include <Windows.h>
+#include <thread>
+#include <conio.h>
+
+using namespace std;
+
+// 전역변수
+int g_Data = 0;
+int g_Connect = 0;
+bool g_Shutdown = false;
+
+CRITICAL_SECTION cs;
+
+void AcceptThread()
+{
+	srand(time(NULL));
+
+	while (!g_Shutdown)
+	{
+		int randNum = rand() % 901 + 100;
+		Sleep(randNum);
+
+		InterlockedIncrement((LONG*)&g_Connect);
+	}
+
+	return;
+}
+
+void DisconnectThread()
+{
+	srand(time(NULL));
+
+	while (!g_Shutdown)
+	{
+		int randNum = rand() % 901 + 100;
+		Sleep(randNum);
+
+		InterlockedDecrement((LONG*)&g_Connect);
+	}
+
+	return;
+}
+
+void UpdateThread()
+{
+	while (!g_Shutdown)
+	{
+		Sleep(10);
+
+		EnterCriticalSection(&cs);
+
+		g_Data++;
+		if (g_Data % 1000 == 0)
+		{
+			cout << g_Data << endl;
+		}
+
+		LeaveCriticalSection(&cs);
+	}
+
+	return;
+}
+
+int main()
+{
+	timeBeginPeriod(1);
+
+	InitializeCriticalSection(&cs);
+
+	HANDLE threadHandles[5];
+	threadHandles[0] = (HANDLE)_beginthreadex(NULL, 0, (_beginthreadex_proc_type)&AcceptThread, NULL, NULL, NULL);
+	Sleep(1000);
+	threadHandles[1] = (HANDLE)_beginthreadex(NULL, 0, (_beginthreadex_proc_type)&DisconnectThread, NULL, NULL, NULL);
+	threadHandles[2] = (HANDLE)_beginthreadex(NULL, 0, (_beginthreadex_proc_type)&UpdateThread, NULL, NULL, NULL);
+	threadHandles[3] = (HANDLE)_beginthreadex(NULL, 0, (_beginthreadex_proc_type)&UpdateThread, NULL, NULL, NULL);
+	threadHandles[4] = (HANDLE)_beginthreadex(NULL, 0, (_beginthreadex_proc_type)&UpdateThread, NULL, NULL, NULL);
+
+	int count = 0;
+	while (1)
+	{
+		Sleep(1000);
+		cout << g_Connect << endl;
+
+		count += 1000;
+		if (count == 20000)
+		{
+			g_Shutdown = true;
+			break;
+		}
+	}
+
+	WaitForMultipleObjects(5, threadHandles, TRUE, INFINITE);
+
+	return 0;
+}
---
+++ 3Cource/ThreadPractice/ThreadTest2/ThreadTest2.cpp
@@ -0,0 +1,159 @@
+﻿#pragma comment(lib, "winmm.lib")
+
+#include <iostream>
+#include <Windows.h>
+#include <thread>
+#include <conio.h>
+#include <list>
+
+using namespace std;
+
+// 전역변수
+list<int> g_list;
+
+// 동기화객체
+SRWLOCK srwLock;
+
+// 이벤트객체
+HANDLE hSaveEvent;
+HANDLE hQuitEvent;
+
+// 쓰레드 핸들
+HANDLE threadHandles[6];
+
+void PrintThread()
+{
+    while (1)
+    {
+        DWORD ret = WaitForSingleObject(hQuitEvent, 1000);
+        if (ret != WAIT_TIMEOUT)
+        {
+            return;
+        }
+
+        AcquireSRWLockShared(&srwLock);
+        list<int>::iterator iter;
+        for (iter = g_list.begin(); iter != g_list.end(); ++iter)
+        {
+            printf("%d - ", *iter);
+        }
+        ReleaseSRWLockShared(&srwLock);
+        printf("\n");
+    }
+}
+
+void DeleteThread()
+{
+    while (1)
+    {
+        DWORD ret = WaitForSingleObject(hQuitEvent, 333);
+        if (ret != WAIT_TIMEOUT)
+        {
+            return;
+        }
+
+        AcquireSRWLockExclusive(&srwLock);
+        if(!g_list.empty())
+            g_list.pop_back();
+        ReleaseSRWLockExclusive(&srwLock);
+    }
+}
+
+void WorkerThread()
+{
+    srand(time(NULL) + GetCurrentThreadId());
+
+    while (1)
+    {
+        DWORD ret = WaitForSingleObject(hQuitEvent, 1000);
+        if (ret != WAIT_TIMEOUT)
+        {
+            return;
+        }
+
+        int randNum = rand();
+        AcquireSRWLockExclusive(&srwLock);
+        g_list.push_front(randNum);
+        ReleaseSRWLockExclusive(&srwLock);
+    }
+}
+
+void SaveThread()
+{
+    HANDLE eventHandles[2];
+
+    eventHandles[0] = hSaveEvent;
+    eventHandles[1] = hQuitEvent;
+
+    while (1)
+    {
+        DWORD ret = WaitForMultipleObjects(2, eventHandles, FALSE, INFINITE);
+        if (ret == WAIT_OBJECT_0 + 1)
+        {
+            return;
+        }
+
+        Sleep(1000);
+        
+        // Lock();
+
+        // 잽싸게 파일에 쓸 데이터들을 지역변수로 저장.
+
+        // UnLock();
+
+        // 독점권 해제 후, 여유롭게 파일 쓰기 시작.
+    }
+}
+
+int main()
+{
+    timeBeginPeriod(1);
+
+    srand(time(NULL));
+    InitializeSRWLock(&srwLock);
+
+    hSaveEvent = CreateEvent(NULL, FALSE, FALSE, NULL);
+    hQuitEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
+
+    if (hSaveEvent == NULL)
+        return 0;
+    if (hQuitEvent == NULL)
+        return 0;
+
+
+    for (int i = 0; i < 10; i++)
+    {
+        g_list.push_back(rand());
+    }
+
+    threadHandles[0] = (HANDLE)_beginthreadex(NULL, 0, (_beginthreadex_proc_type)&PrintThread, NULL, NULL, NULL);
+    threadHandles[1] = (HANDLE)_beginthreadex(NULL, 0, (_beginthreadex_proc_type)&DeleteThread, NULL, NULL, NULL);
+    threadHandles[2] = (HANDLE)_beginthreadex(NULL, 0, (_beginthreadex_proc_type)&WorkerThread, NULL, NULL, NULL);
+    threadHandles[3] = (HANDLE)_beginthreadex(NULL, 0, (_beginthreadex_proc_type)&WorkerThread, NULL, NULL, NULL);
+    threadHandles[4] = (HANDLE)_beginthreadex(NULL, 0, (_beginthreadex_proc_type)&WorkerThread, NULL, NULL, NULL);
+    threadHandles[5] = (HANDLE)_beginthreadex(NULL, 0, (_beginthreadex_proc_type)&SaveThread, NULL, NULL, NULL);
+
+    while (1)
+    {
+        if (_kbhit())
+        {
+            int inputKey = _getch();
+            
+            if (inputKey == 'Q' || inputKey == 'q')
+            {
+                SetEvent(hQuitEvent);
+                break;
+            }
+
+            else if (inputKey == 'S' || inputKey == 's')
+            {
+                SetEvent(hSaveEvent);
+            }
+        }
+    }
+
+
+    WaitForMultipleObjects(6, threadHandles, TRUE, INFINITE);
+
+    return 0;
+}
