fad745328b4362ab25be029191f684635f6ffbf0
yyyoonho <parn997@naver.com>
2025-08-31 21:58:30 +0900

    add: SerializeBufferPtr - 직렬화버퍼를 sharedPtr처럼 랩핑 + 참조카운트

---
+++ 3Cource/SerializeBuffer/SerializeBufferPtr/SerializeBufferPtr.cpp
@@ -0,0 +1,409 @@
+﻿#pragma comment(lib, "SerializeBuffer.lib")
+
+#include <iostream>
+
+#include "Windows.h"
+#include "MemoryPool.h"
+#include "SerializeBuffer.h"
+#include "SerializeBufferPtr.h"
+
+using namespace std;
+
+void RawPtr::IncreaseRefCount()
+{
+	InterlockedIncrement(&_RCBPtr->count);
+}
+
+void RawPtr::DecreaseRefCount()
+{
+    if (InterlockedDecrement(&_RCBPtr->count) == 0)
+    {
+        SerializePacket::SPacketMP.Free(_ptr);
+        delete _RCBPtr;
+
+        _ptr = NULL;
+        _RCBPtr = NULL;
+    }
+}
+
+SerializePacketPtr::SerializePacketPtr()
+{
+    _ptr = NULL;
+    _RCBPtr = NULL;
+}
+
+SerializePacketPtr::SerializePacketPtr(SerializePacket* ptr)
+{
+    _ptr = ptr;
+    _RCBPtr = new RefCountBlock;
+    InterlockedIncrement(&_RCBPtr->count);
+}
+
+SerializePacketPtr::SerializePacketPtr(const SerializePacketPtr& other)
+{
+    InterlockedIncrement(&other._RCBPtr->count);
+
+    _ptr = other._ptr;
+    _RCBPtr = other._RCBPtr;
+}
+
+SerializePacketPtr& SerializePacketPtr::operator=(const SerializePacketPtr& other)
+{
+    LONG ret = InterlockedDecrement(&_RCBPtr->count);
+    if (ret == 0)
+    {
+        SerializePacket::SPacketMP.Free(_ptr);
+
+        _ptr = NULL;
+        _RCBPtr = NULL;
+    }
+
+    InterlockedIncrement(&other._RCBPtr->count);
+    _ptr = other._ptr;
+    _RCBPtr = other._RCBPtr;
+
+    return *this;
+}
+
+SerializePacketPtr::~SerializePacketPtr()
+{
+    if (InterlockedDecrement(&_RCBPtr->count) == 0)
+    {
+        // 찐 소멸
+        SerializePacket::SPacketMP.Free(_ptr);
+        delete _RCBPtr;
+
+        _ptr = NULL;
+        _RCBPtr = NULL;
+    }
+}
+
+void SerializePacketPtr::GetRawPtr(OUT RawPtr* pRaw)
+{
+    pRaw->_ptr = _ptr;
+    pRaw->_RCBPtr = _RCBPtr;
+}
+
+SerializePacket* SerializePacketPtr::MakeSerializePacket()
+{
+    return SerializePacket::SPacketMP.Alloc();
+}
+
+void SerializePacketPtr::Clear()
+{
+    if (_ptr == NULL)
+        return;
+
+    _ptr->Clear();
+}
+
+int SerializePacketPtr::GetBufferSize()
+{
+    if (_ptr == NULL)
+    {
+        return -1;
+    }
+
+    return _ptr->GetBufferSize();
+}
+
+int SerializePacketPtr::GetDataSize()
+{
+    if (_ptr == NULL)
+    {
+        return -1;
+    }
+
+    return _ptr->GetDataSize();
+}
+
+char* SerializePacketPtr::GetBufferPtr()
+{
+    if (_ptr == NULL)
+    {
+        return NULL;
+    }
+
+    return _ptr->GetBufferPtr();
+}
+
+int SerializePacketPtr::MoveWritePos(int size)
+{
+    if (_ptr == NULL)
+    {
+        return -1;
+    }
+
+    return _ptr->MoveWritePos(size);
+}
+
+int SerializePacketPtr::MoveReadPos(int size)
+{
+    if (_ptr == NULL)
+    {
+        return -1;
+    }
+
+    return _ptr->MoveReadPos(size);
+}
+
+SerializePacketPtr& SerializePacketPtr::operator<<(unsigned char byValue)
+{
+    if (_ptr == NULL)
+    {
+        return *this;
+    }
+
+    *_ptr << byValue;
+
+    return *this;
+}
+
+SerializePacketPtr& SerializePacketPtr::operator<<(char chValue)
+{
+    if (_ptr == NULL)
+    {
+        return *this;
+    }
+
+    *_ptr << chValue;
+
+    return *this;
+}
+
+SerializePacketPtr& SerializePacketPtr::operator<<(short shValue)
+{
+    if (_ptr == NULL)
+    {
+        return *this;
+    }
+
+    *_ptr << shValue;
+
+    return *this;
+}
+
+SerializePacketPtr& SerializePacketPtr::operator<<(unsigned short wValue)
+{
+    if (_ptr == NULL)
+    {
+        return *this;
+    }
+
+    *_ptr << wValue;
+
+    return *this;
+}
+
+SerializePacketPtr& SerializePacketPtr::operator<<(int iValue)
+{
+    if (_ptr == NULL)
+    {
+        return *this;
+    }
+
+    *_ptr << iValue;
+
+    return *this;
+}
+
+SerializePacketPtr& SerializePacketPtr::operator<<(long lValue)
+{
+    if (_ptr == NULL)
+    {
+        return *this;
+    }
+
+    *_ptr << lValue;
+
+    return *this;
+}
+
+SerializePacketPtr& SerializePacketPtr::operator<<(float fValue)
+{
+    if (_ptr == NULL)
+    {
+        return *this;
+    }
+
+    *_ptr << fValue;
+
+    return *this;
+}
+
+SerializePacketPtr& SerializePacketPtr::operator<<(DWORD dwValue)
+{
+    if (_ptr == NULL)
+    {
+        return *this;
+    }
+
+    *_ptr << dwValue;
+
+    return *this;
+}
+
+SerializePacketPtr& SerializePacketPtr::operator<<(__int64 iValue)
+{
+    if (_ptr == NULL)
+    {
+        return *this;
+    }
+
+    *_ptr << iValue;
+
+    return *this;
+}
+
+SerializePacketPtr& SerializePacketPtr::operator<<(double dValue)
+{
+    if (_ptr == NULL)
+    {
+        return *this;
+    }
+
+    *_ptr << dValue;
+
+    return *this;
+}
+
+SerializePacketPtr& SerializePacketPtr::operator>>(BYTE& byValue)
+{
+    if (_ptr == NULL)
+    {
+        return *this;
+    }
+
+    *_ptr >> byValue;
+
+    return *this;
+}
+
+SerializePacketPtr& SerializePacketPtr::operator>>(char& chValue)
+{
+    if (_ptr == NULL)
+    {
+        return *this;
+    }
+
+    *_ptr >> chValue;
+
+    return *this;
+}
+
+SerializePacketPtr& SerializePacketPtr::operator>>(short& shValue)
+{
+    if (_ptr == NULL)
+    {
+        return *this;
+    }
+
+    *_ptr >> shValue;
+
+    return *this;
+}
+
+SerializePacketPtr& SerializePacketPtr::operator>>(WORD& wValue)
+{
+    if (_ptr == NULL)
+    {
+        return *this;
+    }
+
+    *_ptr >> wValue;
+
+    return *this;
+}
+
+SerializePacketPtr& SerializePacketPtr::operator>>(int& iValue)
+{
+    if (_ptr == NULL)
+    {
+        return *this;
+    }
+
+    *_ptr >> iValue;
+
+    return *this;
+}
+
+SerializePacketPtr& SerializePacketPtr::operator>>(DWORD& dwValue)
+{
+    if (_ptr == NULL)
+    {
+        return *this;
+    }
+
+    *_ptr >> dwValue;
+
+    return *this;
+}
+
+SerializePacketPtr& SerializePacketPtr::operator>>(float& fValue)
+{
+    if (_ptr == NULL)
+    {
+        return *this;
+    }
+
+    *_ptr >> fValue;
+
+    return *this;
+}
+
+SerializePacketPtr& SerializePacketPtr::operator>>(__int64& iValue)
+{
+    if (_ptr == NULL)
+    {
+        return *this;
+    }
+
+    *_ptr >> iValue;
+
+    return *this;
+}
+
+SerializePacketPtr& SerializePacketPtr::operator>>(double& dValue)
+{
+    if (_ptr == NULL)
+    {
+        return *this;
+    }
+
+    *_ptr >> dValue;
+
+    return *this;
+}
+
+
+int SerializePacketPtr::GetData(char* chpDest, int iSize)
+{
+    if (_ptr == NULL)
+    {
+        return -1;
+    }
+
+    return _ptr->GetData(chpDest, iSize);
+}
+
+int SerializePacketPtr::Putdata(char* chpSrc, int iSrcSize)
+{
+    if (_ptr == NULL)
+    {
+        return -1;
+    }
+
+    return _ptr->Putdata(chpSrc, iSrcSize);
+}
+
+void SerializePacketPtr::PushHeader(char* header, int headerSize)
+{
+    if (_ptr == NULL)
+    {
+        return;
+    }
+
+    _ptr->PushHeader(header, headerSize);
+
+    return;
+}
---
+++ 3Cource/SerializeBuffer/SerializeBufferPtr/SerializeBufferPtr.h
@@ -0,0 +1,95 @@
+#pragma once
+
+// ************************************
+// 샘플 코드
+// 
+// SPacketPtr a(SPacketPtr::MakeSerializePacket());
+// -> 이때, 직렬화버퍼 할당 + RCB 할당이 발생합니다.
+// SPacketPtr b = a;
+// SPacketPtr c;
+// c = a;
+// 
+// ************************************
+
+struct RefCountBlock
+{
+	LONG count = 0;
+};
+
+struct RawPtr
+{
+	//**********************************************
+	// 받았으면 그 즉시, Increse함수를 호출하도록 하자.
+	//**********************************************
+
+	SerializePacket* _ptr;
+	RefCountBlock* _RCBPtr;
+
+	void IncreaseRefCount();
+	void DecreaseRefCount();
+};
+
+class SerializePacketPtr
+{
+public:
+	SerializePacketPtr();
+	SerializePacketPtr(SerializePacket* ptr);
+	SerializePacketPtr(const SerializePacketPtr& other);
+
+	SerializePacketPtr& operator=(const SerializePacketPtr& other);
+
+	~SerializePacketPtr();
+
+public:
+	void GetRawPtr(OUT RawPtr* pRaw);
+
+public:
+	static SerializePacket* MakeSerializePacket();
+
+public:
+	void Clear();
+	int GetBufferSize();
+	int GetDataSize();
+
+	char* GetBufferPtr();
+
+	int MoveWritePos(int size);
+	int MoveReadPos(int size);
+
+	SerializePacketPtr& operator<< (unsigned char byValue);
+	SerializePacketPtr& operator<< (char chValue);
+
+	SerializePacketPtr& operator << (short shValue);
+	SerializePacketPtr& operator << (unsigned short wValue);
+
+	SerializePacketPtr& operator << (int iValue);
+	SerializePacketPtr& operator << (long lValue);
+	SerializePacketPtr& operator << (float fValue);
+	SerializePacketPtr& operator << (DWORD dwValue);
+
+	SerializePacketPtr& operator << (__int64 iValue);
+	SerializePacketPtr& operator << (double dValue);
+
+	SerializePacketPtr& operator >> (BYTE& byValue);
+	SerializePacketPtr& operator >> (char& chValue);
+
+	SerializePacketPtr& operator >> (short& shValue);
+	SerializePacketPtr& operator >> (WORD& wValue);
+
+	SerializePacketPtr& operator >> (int& iValue);
+	SerializePacketPtr& operator >> (DWORD& dwValue);
+	SerializePacketPtr& operator >> (float& fValue);
+
+	SerializePacketPtr& operator >> (__int64& iValue);
+	SerializePacketPtr& operator >> (double& dValue);
+
+
+	int GetData(char* chpDest, int iSize);
+	int Putdata(char* chpSrc, int iSrcSize);
+
+	void PushHeader(char* header, int headerSize);
+
+private:
+	SerializePacket* _ptr;
+	RefCountBlock* _RCBPtr;
+};
