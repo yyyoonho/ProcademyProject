55d82a99097a5682b1cd463c29d7508e1cdf4a32
yyyoonho <parn997@naver.com>
2025-10-14 22:14:28 +0900

    feat: 락프리큐 v1 - 사이즈기반 로직 + 인큐디큐 시, tail 확인후 밀어주기

---
+++ 3Cource/락프리/LockFree_Queue_v1/LockFree_Queue.cpp
@@ -0,0 +1,46 @@
+﻿#include <Windows.h>
+#include <thread>
+#include <iostream>
+
+#include "MemoryPool.h"
+#include "LockFree_Queue.h"
+
+using namespace std;
+
+LockFreeQueue<int> myQueue;
+
+void Thread()
+{
+	while (1)
+	{
+		for (int i = 0; i < 1; i++)
+		{
+			bool ret = myQueue.Enqueue(i);
+			if (ret == false)
+			{
+				DebugBreak();
+			}
+		}
+
+		for (int i = 0; i < 1; i++)
+		{
+			int tmp;
+			bool ret = myQueue.Dequeue(&tmp);
+			if (ret == false)
+			{
+				DebugBreak();
+			}
+		}
+	}
+}
+
+int main()
+{
+	HANDLE hThreads[3];
+	for (int i = 0; i < 3; i++)
+	{
+		hThreads[i] = (HANDLE)_beginthreadex(NULL, 0, (_beginthreadex_proc_type)&Thread, NULL, NULL, NULL);
+	}
+
+	WaitForMultipleObjects(3, hThreads, TRUE, INFINITE);
+}
