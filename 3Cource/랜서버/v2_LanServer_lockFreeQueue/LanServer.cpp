747bc80fb1fed83fd9138f88b4436145ca717cb1
yyyoonho <parn997@naver.com>
2025-10-15 01:17:23 +0900

    feat: 락프리큐 추가한 랜서버 분리

---
+++ 3Cource/랜서버/v2_LanServer_lockFreeQueue/LanServer.cpp
@@ -0,0 +1,527 @@
+﻿#include "stdafx.h"
+#include "LanServer.h"
+
+using namespace std;
+CLanServer::CLanServer()
+{
+}
+
+CLanServer::~CLanServer()
+{
+}
+
+bool CLanServer::Start(const WCHAR* ipAddress, unsigned short port, unsigned short workerThreadCount, unsigned short coreSkip, bool isNagle, unsigned int maximumSessionCount)
+{
+	_ipAddress = ipAddress;
+	_port = port;
+	_workerThreadCount = workerThreadCount;
+	_coreSkip = coreSkip;
+	_isNagle = isNagle;
+	_maximumSessionCount = maximumSessionCount;
+
+	_hEvent_Quit = CreateEvent(NULL, TRUE, FALSE, NULL);
+
+	InitializeSRWLock(&_releaseStackLock);
+
+	for (int i = 19999; i >= 0; i--)
+	{
+		_sessionArray[i].recvQ.Resize(20000);
+		//_sessionArray[i].sendQ.Resize(20000);
+		//InitializeSRWLock(&_sessionArray[i].sendQLock);
+
+		_releaseIdxStack.push(i);
+	}
+
+	bool ret = NetInit();
+	if (ret == false)
+		return false;
+
+	_hThread_Accept = (HANDLE)_beginthreadex(NULL, 0, (_beginthreadex_proc_type)&CLanServer::AcceptThreadRun, this, NULL, NULL);
+	if (_hThread_Accept == NULL)
+		return false;
+
+	_hThread_Monitor = (HANDLE)_beginthreadex(NULL, 0, (_beginthreadex_proc_type)&CLanServer::MonitorThreadRun, this, NULL, NULL);
+	if (_hThread_Monitor == NULL)
+		return false;
+
+	SYSTEM_INFO si;
+	GetSystemInfo(&si);
+
+	_hIOCP = CreateIoCompletionPort(INVALID_HANDLE_VALUE, NULL, 0, (int)si.dwNumberOfProcessors - coreSkip);
+	if (_hIOCP == NULL)
+		return false;
+
+	HANDLE hThread;
+	for (int i = 0; i < workerThreadCount; i++)
+	{
+		hThread = (HANDLE)_beginthreadex(NULL, 0, (_beginthreadex_proc_type)&CLanServer::WorkerThreadRun, this, NULL, NULL);
+		if (hThread == NULL)
+			return false;
+		_workerThreadHandles.push_back(hThread);
+	}
+
+	return true;
+}
+
+void CLanServer::Stop()
+{
+	SetEvent(_hEvent_Quit);
+	PostQueuedCompletionStatus(_hIOCP, NULL, NULL, NULL);
+}
+
+int CLanServer::GetSessionCount()
+{
+	return _sessionCount;
+}
+
+bool CLanServer::Disconnect(DWORD64 sessionID)
+{
+	return false;
+}
+
+void CLanServer::SendPost(Session* pSession)
+{
+	if (pSession->LockFreeSendQ.Size() > 0)
+	{
+		// 여기서 0됨
+
+		if (InterlockedExchange(&pSession->checkSend, false) == true)
+		{
+			SendProc(pSession);
+		}
+	}
+}
+
+bool CLanServer::SendPacket(DWORD64 sessionID, SerializePacketPtr pPacket)
+{
+	unsigned int idx = GetIdxFromSessionID(sessionID);
+
+	Session* pSession = NULL;
+	pSession = &_sessionArray[idx];
+
+	if (sessionID != pSession->sessionID)
+	{
+		return false;
+	}
+
+	stHeader header;
+	header.len = pPacket.GetDataSize();
+
+	pPacket.PushHeader((char*)&header, sizeof(stHeader));
+
+	//pSession->sendQ.push(pPacket);
+	pSession->LockFreeSendQ.Enqueue(pPacket);
+
+	InterlockedIncrement(&_sendMessageTPS);
+
+	SendPost(pSession);
+
+	return true;
+}
+
+bool CLanServer::NetInit()
+{
+	WSADATA wsaData;
+	wsaStartupRet = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (wsaStartupRet != 0)
+		return false;
+
+	_listenSocket = socket(AF_INET, SOCK_STREAM, 0);
+	if (_listenSocket == INVALID_SOCKET)
+	{
+		printf("Error: listen socket()\n");
+		return false;
+	}
+
+	int optVal = 0;
+	setSockOptRet = setsockopt(_listenSocket, SOL_SOCKET, SO_SNDBUF, (char*)&optVal, sizeof(optVal));
+	if (setSockOptRet == SOCKET_ERROR)
+	{
+		printf("ERROR: setsockopt() %d\n", GetLastError());
+		return false;
+	}
+
+	SOCKADDR_IN serverAddr;
+	memset(&serverAddr, 0, sizeof(SOCKADDR_IN));
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(_port);
+	serverAddr.sin_addr.S_un.S_addr = htonl(INADDR_ANY);
+
+	bindRet = bind(_listenSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr));
+	if (bindRet == SOCKET_ERROR)
+	{
+		printf("Error: bind()\n");
+		return false;
+	}
+
+	listenRet = listen(_listenSocket, SOMAXCONN);
+	if (listenRet == SOCKET_ERROR)
+	{
+		printf("Error: listen()\n");
+		return false;
+	}
+}
+
+void CLanServer::RecvProc(Session* pSession)
+{
+	DWORD sendBytes;
+	DWORD flags = 0;
+
+	WSABUF wsaBuf;
+	wsaBuf.buf = pSession->recvQ.GetRearBufferPtr();
+	wsaBuf.len = pSession->recvQ.DirectEnqueueSize();
+
+	IncreaseIO_Count(pSession);
+
+	DWORD wsaRecvRet = WSARecv(pSession->sock, &wsaBuf, 1, &sendBytes, &flags, (LPWSAOVERLAPPED)&pSession->recvMyOverlapped, NULL);
+	if (wsaRecvRet == SOCKET_ERROR)
+	{
+		if (WSAGetLastError() != ERROR_IO_PENDING)
+		{
+			if (WSAGetLastError() != 0)
+			{
+				DecreaseIO_Count(pSession);
+			}
+
+			printf("Error: WSARecv() %d\n", WSAGetLastError());
+			return;
+		}
+	}
+}
+
+void CLanServer::SendProc(Session* pSession)
+{
+	//if (pSession->sendQ.DirectDequeueSize() > 0)
+	{
+		WSABUF wsaBuf[100];
+		int sPacketCount = 0;
+		for (int i = 0; i < 100; i++)
+		{
+			if (pSession->LockFreeSendQ.Size() <=  0)
+				break;
+
+			SerializePacketPtr pPacket;
+			//pPacket = pSession->sendQ.front();
+			//pSession->sendQ.pop();
+			pSession->LockFreeSendQ.Dequeue(&pPacket);
+
+			wsaBuf[i].buf = pPacket.GetBufferPtr();
+			wsaBuf[i].len = pPacket.GetDataSize();
+
+			pSession->sendMyOverlapped.sendSerializePacketPtrArr[i] = pPacket;
+
+			sPacketCount++;
+		}
+
+		pSession->sendMyOverlapped.sPacketCount = sPacketCount;
+
+		IncreaseIO_Count(pSession);
+
+		DWORD wsaSendRet = WSASend(pSession->sock, wsaBuf, sPacketCount, NULL, 0, (LPWSAOVERLAPPED)&pSession->sendMyOverlapped, NULL);
+		DWORD tmp2 = WSAGetLastError();
+		if (wsaSendRet == SOCKET_ERROR)
+		{
+			DWORD tmp = WSAGetLastError();
+
+			if (WSAGetLastError() != ERROR_IO_PENDING && WSAGetLastError() != 0)
+			{
+				DecreaseIO_Count(pSession);
+
+				printf("Error: WSASend() %d\n", WSAGetLastError());
+				return;
+			}
+		}
+	}
+}
+
+void CLanServer::IncreaseIO_Count(Session* pSession)
+{
+	InterlockedIncrement(&(pSession->IO_Count));
+}
+
+void CLanServer::DecreaseIO_Count(Session* pSession)
+{
+	LONG ret = InterlockedDecrement(&(pSession->IO_Count));
+	if (ret == 0)
+	{
+		// Session Release
+
+		closesocket(pSession->sock);
+		
+		unsigned int idx = GetIdxFromSessionID(pSession->sessionID);
+
+		AcquireSRWLockExclusive(&_releaseStackLock);
+
+		_releaseIdxStack.push(idx);
+
+		ReleaseSRWLockExclusive(&_releaseStackLock);
+
+		InterlockedDecrement(&_sessionCount);
+
+		OnRelease(pSession->sessionID);
+	}
+}
+
+unsigned int CLanServer::GetIdxFromSessionID(DWORD64 sessionID)
+{
+	unsigned int idx = (sessionID & 0xffff000000000000) >> 48;
+
+	return idx;
+}
+
+void CLanServer::SetIdxToSessionID(DWORD64* pSessionID, unsigned int idx)
+{
+	DWORD64 sessionID = *pSessionID;
+
+	DWORD64 shiftedIdx = (DWORD64)idx << 48;
+
+	*pSessionID = sessionID | shiftedIdx;
+
+	return;
+}
+
+void CLanServer::WorkerThreadRun(LPVOID* lParam)
+{
+	CLanServer* self = (CLanServer*)lParam;
+	self->WorkerThread();
+}
+
+void CLanServer::WorkerThread()
+{
+	while (1)
+	{
+		// 비동기 IO 완료통지 대기
+		DWORD cbTransferred = NULL;
+		Session* pSession = NULL;
+		myOverlapped* pMyOverlapped = NULL;
+
+		SOCKADDR_IN clientAddr;
+		int addrLen = sizeof(clientAddr);
+		WCHAR addrBuf[40];
+
+		BOOL retVal = GetQueuedCompletionStatus(_hIOCP, &cbTransferred, (PULONG_PTR)&pSession, (LPWSAOVERLAPPED*)&pMyOverlapped, INFINITE);
+		if (pMyOverlapped == NULL && cbTransferred == NULL && pSession == NULL)
+		{
+			// 종료통지 or IOCP 결함
+			// => 워커쓰레드 종료.
+			PostQueuedCompletionStatus(_hIOCP, NULL, NULL, NULL);
+
+			return;
+		}
+
+		if (cbTransferred == 0)
+		{
+			// 클라에서 FIN or RST 를 던졌다.
+			getpeername(pSession->sock, (SOCKADDR*)&clientAddr, &addrLen);
+			InetNtop(AF_INET, &clientAddr.sin_addr, addrBuf, 40);
+
+			//printf("\n[TCP 서버] 클라이언트 종료신호 (FIN or SRT): IP주소=%ls, 포트번호=%d\n", addrBuf, ntohs(clientAddr.sin_port));
+		}
+
+		else if (pMyOverlapped->type == RECV)
+		{
+			if (pSession->loginCheck == FALSE)
+			{
+				DecreaseIO_Count(pSession);
+				pSession->loginCheck = TRUE;
+			}
+
+			pSession->recvQ.MoveRear(cbTransferred);
+
+			while (1)
+			{
+				stHeader header;
+
+				if (pSession->recvQ.GetUseSize() < sizeof(stHeader))
+				{
+					// 비동기IO: Recv 요청 후 break
+					RecvProc(pSession);
+					break;
+				}
+
+				pSession->recvQ.Peek((char*)&header, sizeof(stHeader));
+				short payLoadLen = header.len;
+
+				if (pSession->recvQ.GetUseSize() < sizeof(stHeader) + payLoadLen)
+				{
+					// 비동기IO: Recv 요청 후 break
+					RecvProc(pSession);
+					break;
+				}
+
+				pSession->recvQ.MoveFront(sizeof(header));
+
+				//SerializePacket sPacket;
+				SerializePacketPtr pPacket = SerializePacketPtr::MakeSerializePacket();
+				pPacket.Clear();
+
+				//int ret = pSession->recvQ.Dequeue(sPacket.GetBufferPtr(), payLoadLen);
+				//sPacket.MoveWritePos(ret);
+				int ret = pSession->recvQ.Dequeue(pPacket.GetBufferPtr(), payLoadLen);
+				pPacket.MoveWritePos(ret);
+
+				InterlockedIncrement(&_recvMessageTPS);
+
+				// 비동기IO: Send 요청
+				// 이제 컨텐츠 단에서 요청
+				OnMessage(pSession->sessionID, pPacket);
+			}
+		}
+
+		else if (pMyOverlapped->type == SEND)
+		{
+			//pSession->sendQ.MoveFront(cbTransferred);
+			
+			for (int i = 0; i < pMyOverlapped->sPacketCount; i++)
+			{
+				pMyOverlapped->sendSerializePacketPtrArr[i] = NULL;
+			}
+
+			//AcquireSRWLockExclusive(&pSession->sendQLock);
+
+			// 비동기IO: Send 요청
+			if (pSession->LockFreeSendQ.Size() > 0)
+			{
+				SendProc(pSession);
+			}
+			else
+			{
+				InterlockedExchange(&pSession->checkSend, TRUE);
+			}
+
+
+			//ReleaseSRWLockExclusive(&pSession->sendQLock);
+		}
+
+		DecreaseIO_Count(pSession);
+	}
+}
+
+void CLanServer::AcceptThreadRun(LPVOID* lParam)
+{
+	CLanServer* self = (CLanServer*)lParam;
+	self->AcceptThread();
+}
+
+void CLanServer::AcceptThread()
+{
+	while (1)
+	{
+		SOCKADDR_IN clientAddr;
+		memset(&clientAddr, 0, sizeof(SOCKADDR_IN));
+		int clientAddrSize = sizeof(clientAddr);
+
+		SOCKET clientSocket = accept(_listenSocket, (SOCKADDR*)&clientAddr, &clientAddrSize);
+		if (clientSocket == INVALID_SOCKET)
+		{
+			printf("Error: accept() %d\n", WSAGetLastError());
+			return;
+		}
+
+		InterlockedIncrement(&_acceptTPS);
+
+		// OnConnectionRequest
+		OnConnectionRequest(clientAddr);
+
+		// 세션 할당 및 초기화
+		AcquireSRWLockExclusive(&_releaseStackLock);
+
+		int idx = _releaseIdxStack.top();
+		_releaseIdxStack.pop();
+
+		ReleaseSRWLockExclusive(&_releaseStackLock);
+
+		_sessionArray[idx].sock = clientSocket;
+		_sessionArray[idx].sessionID = ++_g_sessionID;
+
+		SetIdxToSessionID(&_sessionArray[idx].sessionID, idx);
+
+		unsigned int tmp = GetIdxFromSessionID(_sessionArray[idx].sessionID);
+
+		memset(&(_sessionArray[idx].recvMyOverlapped.overlapped), 0, sizeof(WSAOVERLAPPED));
+		_sessionArray[idx].recvMyOverlapped.pSession = &_sessionArray[idx];
+		_sessionArray[idx].recvMyOverlapped.type = RECV;
+
+		memset(&(_sessionArray[idx].sendMyOverlapped.overlapped), 0, sizeof(WSAOVERLAPPED));
+		_sessionArray[idx].sendMyOverlapped.pSession = &_sessionArray[idx];
+		_sessionArray[idx].sendMyOverlapped.type = SEND;
+
+		for (int i = 0; i < 100; i++)
+		{
+			_sessionArray[idx].sendMyOverlapped.sendSerializePacketPtrArr[i] = NULL;
+		}
+
+		_sessionArray[idx].sendMyOverlapped.sPacketCount = 0;
+
+		_sessionArray[idx].recvQ.ClearBuffer();
+		while (_sessionArray[idx].LockFreeSendQ.Size() > 0)
+		{
+			//_sessionArray[idx].sendQ.pop();
+			_sessionArray[idx].LockFreeSendQ.Clear();
+		}
+
+		_sessionArray[idx].loginCheck = FALSE;
+
+		_sessionArray[idx].checkSend = TRUE;
+
+		getpeername(clientSocket, (SOCKADDR*)&clientAddr, &clientAddrSize);
+		WCHAR addrBuf[40];
+		InetNtop(AF_INET, &clientAddr.sin_addr, addrBuf, 40);
+		//printf("\n[TCP 서버] 클라이언트 접속: IP주소=%ls, 포트번호=%d\n", addrBuf, ntohs(clientAddr.sin_port));
+
+		InterlockedIncrement(&_sessionCount);
+
+		// 소켓 <-> IOCP 연결
+		CreateIoCompletionPort((HANDLE)_sessionArray[idx].sock, _hIOCP, (ULONG_PTR)&_sessionArray[idx], NULL);
+
+		// OnAccept
+		{
+			IncreaseIO_Count(&_sessionArray[idx]);
+			OnAccept(_sessionArray[idx].sessionID);
+		}
+
+		// 비동기 IO 걸어두기
+		RecvProc(&_sessionArray[idx]);
+	}
+}
+
+void CLanServer::MonitorThreadRun(LPVOID* lParam)
+{
+	CLanServer* self = (CLanServer*)lParam;
+	self->MonitorThread();
+}
+
+void CLanServer::MonitorThread()
+{
+	while (1)
+	{
+		DWORD ret = WaitForSingleObject(_hEvent_Quit, 1000);
+		if (ret == WAIT_OBJECT_0)
+		{
+			return;
+		}
+
+		_acceptTPS_BackUp = _acceptTPS;
+		_recvMessageTPS_BackUp = _recvMessageTPS;
+		_sendMessageTPS_BackUp = _sendMessageTPS;
+
+		InterlockedExchange(&_acceptTPS, 0);
+		InterlockedExchange(&_recvMessageTPS, 0);
+		InterlockedExchange(&_sendMessageTPS, 0);
+	}
+}
+
+int CLanServer::GetAcceptTPS()
+{
+	return _acceptTPS_BackUp;
+}
+
+int CLanServer::GetRecvMessageTPS()
+{
+	return _recvMessageTPS_BackUp;
+}
+
+int CLanServer::GetSendMessageTPS()
+{
+	return _sendMessageTPS_BackUp;
+}
