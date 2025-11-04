d8a4e6f28ddbaf795feceb1abd2592349b0922ec
yyyoonho <parn997@naver.com>
2025-11-05 01:04:10 +0900

    feat: LanServer Release 구현 (IO Count, release flag 사용)

---
+++ 4Cource/랜서버/v3.2.2_LanServer_Release/EchoServer.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#define ECHOTHREADCOUNT 3
+
+class CEchoServer : public CLanServer
+{
+public:
+	virtual bool Start(const WCHAR* ipAddress,
+		unsigned short port,
+		unsigned short workerThreadCount,
+		unsigned short coreSkip,
+		bool isNagle,
+		unsigned int maximumSessionCount) override;
+
+	virtual void Stop() override;
+
+public:
+	virtual bool OnConnectionRequest(SOCKADDR_IN clientAddr) override;
+
+	virtual void OnAccept(DWORD64 sessionID) override;
+
+	virtual void OnRelease(DWORD64 sessionID) override;
+
+	virtual void OnMessage(DWORD64 sessionID, SerializePacketPtr pPacket) override;
+
+	virtual void OnError(int errorCode, WCHAR* errorComment) override;
+
+private:
+	static void ContentThreadRun(LPVOID* lParam);
+	void ContentThread();
+
+private:
+	// ¸â¹ö º¯¼ö: ÄÜÅÙÃ÷ Å¥, ÀÌº¥Æ®
+	RingBuffer _contentQueue[ECHOTHREADCOUNT];
+	HANDLE _hEvent_contentQueue[ECHOTHREADCOUNT];
+
+	SRWLOCK _contentQueueLock[ECHOTHREADCOUNT];
+
+	HANDLE _hThread_EchoThread[ECHOTHREADCOUNT];
+	std::unordered_map<DWORD, int> threadQueueMap;
+
+private:
+	// ¸â¹öº¯¼ö: ÀÌº¥Æ®
+	HANDLE _hEvent_Quit;
+};
