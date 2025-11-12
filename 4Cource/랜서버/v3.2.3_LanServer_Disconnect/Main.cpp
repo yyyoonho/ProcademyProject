059c810aab764cbe20b09733c6ea2b8388d099d9
yyyoonho <parn997@naver.com>
2025-11-12 15:09:44 +0900

    fix: Disconnect 기능 수정 및 버전추가

---
+++ 4Cource/랜서버/v3.2.3_LanServer_Disconnect/Main.cpp
@@ -0,0 +1,73 @@
+#include "stdafx.h"
+#include "LanServer.h"
+#include "EchoServer.h"
+
+using namespace std;
+
+CEchoServer server1;
+
+HANDLE hMonitoringThread;
+HANDLE hEvent_Quit;
+
+int a = 1;
+
+void MonitoringThread()
+{
+	while (1)
+	{
+		DWORD ret = WaitForSingleObject(hEvent_Quit, 1000);
+		if (ret == WAIT_OBJECT_0)
+		{
+			return;
+		}
+
+		printf("-----------------------------------\n");
+		printf("sessionCount : %d\n\n", server1.GetSessionCount());
+
+		printf("acceptTPS : %d\n", server1.GetAcceptTPS());
+		printf("recvMessageTPS : %d\n", server1.GetRecvMessageTPS());
+		printf("sendMessageTPS : %d\n", server1.GetSendMessageTPS());
+		printf("-----------------------------------\n");
+		printf("\n");
+	}
+}
+
+int main()
+{
+	bool serverSet = server1.Start(L"127.0.0.1", 6000, 2, 2, TRUE, 10000);
+	if (serverSet == false)
+	{
+		printf("server start error\n");
+		return 0;
+	}
+
+	hEvent_Quit = CreateEvent(NULL, TRUE, FALSE, NULL);
+
+	hMonitoringThread = (HANDLE)_beginthreadex(NULL, 0, (_beginthreadex_proc_type) & MonitoringThread, NULL, NULL, NULL);
+	if (hMonitoringThread == NULL)
+	{
+		printf("hMonitoringThread create error\n");
+		return 0;
+	}
+
+	while (1)
+	{
+		char inputKey = _getch();
+		if (inputKey == 'Q' || inputKey == 'q')
+		{
+			server1.Stop();
+			SetEvent(hEvent_Quit);
+
+			break;
+		}
+
+		if (inputKey == 'K' || inputKey == 'k')
+		{
+			server1.Disconnect(1);
+		}
+	}
+
+	WaitForSingleObject(hMonitoringThread, INFINITE);
+
+	return 0;
+}
