2223dbd22fc4bef4affb15971f6ba04e61f0358f
yyyoonho <parn997@naver.com>
2025-12-09 23:44:44 +0900

    feat: 멀티스레드 채팅서버 - 네트워크 유틸 lib→cpp 전환, cancelIOFlag 초기화 추가

---
+++ 4Cource/ChattingServer/NetServer_v2.0_multiThread/Monitoring.h
@@ -0,0 +1,53 @@
+#pragma once
+// 모니터링 항목
+enum class MonitorType : int
+{
+	PacketPool_FULL,		// Packet 풀 할당량 (아직)				()	-> 초기화 x
+	RCBPool_FULL,			// RCB 구조체 할당량 (아직)				()	-> 초기화 x
+	PacketPool_EMPTY,		// Packet 풀 할당량 (아직)				()	-> 초기화 x
+	RCBPool_EMPTY,			// RCB 구조체 할당량 (아직)				()	-> 초기화 x
+
+	PacketUseCount,
+
+	SessionNum,				// NetServer 의 세션수					(O) -> 초기화 x
+	PlayerCount,			// Contents 파트 Player 수				(O)	-> 초기화 x
+
+	AcceptTotal,			// Accept 전체 카운트 (accept 리턴시 +1)	(O)	-> 초기화 x
+	AcceptTPS,				// Accept 처리 횟수						(O)
+	UpdateTPS,				// Update 처리 초당 횟수					(O)
+
+	RecvMessageTPS,			// 초당 받은 메시지 수					(O)
+	SendMessageTPS,			// 초당 보낸 메시지 수					(O)
+
+	RecvMessageLoginTPS,	// 초당 받은 메시지 수 (login)			(O)
+	RecvMessageMoveTPS,		// 초당 받은 메시지 수 (move)				(O)
+	RecvMessageChatTPS,		// 초당 받은 메시지 수 (chat)				(O)
+
+	DisconnectTotal_alreadyLogin,
+
+	COUNT,
+};
+
+class Monitoring
+{
+private:
+	static Monitoring* _pMonitoring;
+
+private:
+	Monitoring();
+	~Monitoring();
+
+public:
+	static Monitoring* GetInstance();
+	void Increase(MonitorType type);
+	void IncreaseInterlocked(MonitorType type);
+	void Decrease(MonitorType type);
+	void DecreaseInterlocked(MonitorType type);
+	void PrintMonitoring();
+	void Clear();
+
+private:
+	LONG _monitoringArr[(int)MonitorType::COUNT];
+	
+};
+
